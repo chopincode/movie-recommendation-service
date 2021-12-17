b8b03f8967ed4b77ac93260e8f67770b1adb93f1
Seiya T <tajimast@amazon.com>
2021-12-17 10:49:07 -0800

    Using Axios to send HTTP request

--- app/app.js
+++ app/app.js
@@ -1,21 +1,22 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const axios = require('axios')
 
+//TODO: Use Axios to send the HTTP request to catalog service to obtain the movies
 app.get('/recommendations', (req, res) => {
-  res.send({
-    result: {
+  res.send({result: {
       movies: [
         {
-          movieName: "Harry Poter",
+          movieName: "Harry Potter",
           movieImageURL: "www.google.com"
         },
         {
-          movieName: "Harry Poter2",
+          movieName: "Harry Potter2",
           movieImageURL: "www.facebook.com"
         },
         {
-          movieName: "Harry Poter3",
+          movieName: "Harry Potter3",
           movieImageURL: "www.amazon.com"
         },
       ],
